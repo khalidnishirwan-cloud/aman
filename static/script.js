a5360bc29e528cce0fa9d41ea8b4f58a7b0bd4a8
khalidnishirwan-cloud <khalidnishirwan@gmail.com>
2026-03-20 04:45:16 +0300

    Add message sending and chat functions

--- static/script.js
+++ static/script.js
@@ -1,1 +1,43 @@
+function sendMessage() {
+    const input = document.getElementById("message");
+    const msg = input.value;
 
+    if (!msg) return;
+
+    addMessage(msg, "user");
+
+    fetch('/ask', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message: msg })
+    })
+    .then(res => res.json())
+    .then(data => {
+        addMessage(data.response, "bot");
+    });
+
+    input.value = "";
+}
+
+function addMessage(text, type) {
+    const box = document.getElementById("chat-box");
+
+    const div = document.createElement("div");
+    div.className = "message " + type;
+    div.innerText = text;
+
+    box.appendChild(div);
+    box.scrollTop = box.scrollHeight;
+}
+
+function newChat() {
+    document.getElementById("chat-box").innerHTML = "";
+}
+
+function goLogin() {
+    window.location.href = "/login";
+}
+
+function goRegister() {
+    window.location.href = "/register";
+}
